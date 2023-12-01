de966c06a3d883edc9b61d56dcad7640ab4816de
dapatterson20 <dapatterson20@students.desu.edu>
2023-12-01 13:30:49 +0000

    First changes

---
+++ paging.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct pageTable {
+    int frame[256];
+    int validBit[256];
+}pageTable;
+
+typedef struct translationLookasideBuffer {
+    int pageNumber[16];
+    int frame[16];
+}translationLookasideBuffer;
+
+void read_from_file(char* filename){
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+    
+    int* page = NULL;
+
+    page=(int*)malloc(sizeof(int*)*15);
+
+    int pageNumber;
+    int offset;
+
+    for (int i=0; i<15; i++) {
+        fscanf(fp, "%d\n", &page[i]);
+        pageNumber=(page[i]>>8)&0x00FF;
+        offset=page[i]&0x00FF;
+        printf("%d\n",page[i]);
+        printf("%d\n",pageNumber);
+        printf("%d\n",offset);
+    }
+}
+
+void readBinary(char* filename) {
+    FILE *fp = fopen(filename, "r");
+    if (!fp) {
+        perror("fopen");
+        exit(EXIT_FAILURE);
+    }
+    int a;
+    int* binary=(int*)malloc(sizeof(int*)*256);
+    for (int x=0; x<256; x++) {
+        a=fscanf(fp, "%d\n", &binary[x]);
+        printf("Binary: %d\n", a);
+    }
+
+}
+
+void findFrame(int pageNum, int offset) {
+    translationLookasideBuffer tlb;
+    pageTable pTable;
+    bool inTLB;
+    for (int x=0; x<16; x++) {
+        if (tlb.pageNumber[x]==pageNum) {
+            inTLB=true;
+            break;
+        }
+    }
+    if (!inTLB) {
+        if (pTable.validBit[pageNum]==0) {
+            //Page fault; consult backing store.
+        }
+    }
+}
+
+int main() {
+    read_from_file("addresses.txt");
+    readBinary("BACKING_STORE.bin");
+    return 0;
+}
